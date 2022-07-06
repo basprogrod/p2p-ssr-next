e9db662924363cdc7b21a58b28454b7c220b79d2
Igor Vasilyeu <ivasilyeu@linkando.com>
2022-07-06 10:45:50 +0300

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,95 +1,114 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { FC, SyntheticEvent, useEffect, useState } from 'react'
-import { DataConnection, Peer } from 'peerjs'
-import axios from 'axios'
+import { FC, SyntheticEvent, useEffect, useState } from "react";
+import { DataConnection, Peer } from "peerjs";
+import axios from "axios";
 
 interface HomeProps {
-  current: string
-  other: string[]
-  initiator: string
-  init: boolean
-  electionItems: string[]
+  current: string;
+  other: string[];
+  initiator: string;
+  init: boolean;
+  electionItems: string[];
 }
 
-const Home: FC<HomeProps> = ({ electionItems, current, other, initiator, init }) => {
-  const [peer, setPeer] = useState<Peer | undefined>(undefined)
-  const [connection, setConnection] = useState<DataConnection | undefined>(undefined)
-  const [numbers, setNumbers] = useState<number[]>([])
-  const [result, setResult] = useState<string[]>([])
-  const [isOpen, setIsOpen] = useState(false)
-  const [isVoted, setIsVoted] = useState(false)
-  const [connectedUsers, setConnectedUsers] = useState<string[]>([])
+const Home: FC<HomeProps> = ({
+  electionItems,
+  current,
+  other,
+  initiator,
+  init,
+}) => {
+  const [peer, setPeer] = useState<Peer | undefined>(undefined);
+  const [connection, setConnection] = useState<DataConnection | undefined>(
+    undefined
+  );
+  const [numbers, setNumbers] = useState<number[]>([]);
+  const [result, setResult] = useState<string[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isVoted, setIsVoted] = useState(false);
+  const [connectedUsers, setConnectedUsers] = useState<string[]>([]);
 
   useEffect(() => {
-    if (!numbers.length) return
+    if (!numbers.length) return;
 
-    const init = electionItems.reduce<{ [field: number]: number }>((acc, cur, i) => {
-      acc[i] = 0
-      return acc
-    }, {})
+    const init = electionItems.reduce<{ [field: number]: number }>(
+      (acc, cur, i) => {
+        acc[i] = 0;
+        return acc;
+      },
+      {}
+    );
 
-    const preparedData = numbers.reduce<{ [field: number]: number }>((acc, cur) => {
-      if (!!acc[cur]) acc[cur]++
-      else acc[cur] = 1
+    const preparedData = numbers.reduce<{ [field: number]: number }>(
+      (acc, cur) => {
+        if (!!acc[cur]) acc[cur]++;
+        else acc[cur] = 1;
 
-      return acc
-    }, init)
+        return acc;
+      },
+      init
+    );
 
-    const r = Object.keys(preparedData).map(key => {
-      return ((preparedData[+key] / numbers.length) * 100).toFixed(2)
-    })
+    const r = Object.keys(preparedData).map((key) => {
+      return ((preparedData[+key] / numbers.length) * 100).toFixed(2);
+    });
 
-    setResult(r)
-  }, [numbers])
-
-  useEffect(() => {
-    import('peerjs').then(({ default: Peer }) => {
-      const newPeer = new Peer(init ? initiator : current, { debug: 2 })
-
-      newPeer.on('open', () => {
-        console.log('🚀 ~ open')
-        setIsOpen(true)
-      })
-
-      newPeer.on('connection', conn => {
-        conn.on('data', data => {
-          console.log(data)
-          setNumbers(state => [...state, +(data as string)])
-        })
-        setConnectedUsers(state => [...state, conn.peer])
-      })
-
-      setPeer(newPeer)
-    })
-  }, [])
+    setResult(r);
+  }, [numbers]);
 
   useEffect(() => {
-    if (!peer) return
-    if (init) return
+    import("peerjs").then(({ default: Peer }) => {
+      const newPeer = new Peer(init ? initiator : current, { debug: 2 });
 
-    const conn = peer.connect(initiator)
+      newPeer.on("open", () => {
+        console.log("🚀 ~ open");
+        setIsOpen(true);
+      });
 
-    conn.on('open', () => {
-      console.log('connection!!!')
-    })
+      newPeer.on("connection", (conn) => {
+        conn.on("data", (data) => {
+          console.log(data);
+          setNumbers((state) => [...state, +(data as string)]);
+        });
+        setConnectedUsers((state) => [...state, conn.peer]);
+      });
 
-    setConnection(conn)
-  }, [isOpen])
+      setPeer(newPeer);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!peer) return;
+    if (init) return;
+
+    const conn = peer.connect(initiator);
+
+    conn.on("open", () => {
+      console.log("connection!!!");
+    });
+
+    setConnection(conn);
+  }, [isOpen]);
 
   const sendData = (e: SyntheticEvent<HTMLButtonElement>) => {
-    setIsVoted(true)
-    connection!.send(e.currentTarget.dataset.electionId)
-  }
+    setIsVoted(true);
+    connection!.send(e.currentTarget.dataset.electionId);
+  };
 
   useEffect(() => {
-    if (!peer) return
-    ;(window as any).peer = peer
-  }, [peer])
+    if (!peer) return;
+    (window as any).peer = peer;
+  }, [peer]);
 
   return (
     <div className="Home">
       {electionItems.map((name, i) => (
-        <button key={name} disabled={isVoted} data-election-id={i} onClick={sendData}>
+        <button
+          key={name}
+          disabled={isVoted}
+          data-election-id={i}
+          onClick={sendData}
+        >
           {name}
         </button>
       ))}
@@ -113,20 +132,20 @@ const Home: FC<HomeProps> = ({ electionItems, current, other, initiator, init })
         <div key={id}>{id}</div>
       ))} */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-export async function getServerSideProps({ query }) {
-  const res = await axios.get('http://localhost:3000/api/get-users')
-  console.log('🚀 ~ file: index.tsx ~ line 94 ~ res', res.data)
+export async function getServerSideProps({ query }: any) {
+  const res = await axios.get("http://localhost:3000/api/get-users");
+  console.log("🚀 ~ file: index.tsx ~ line 94 ~ res", res.data);
 
   return {
     props: {
       ...res.data,
       init: !!query.init,
-      electionItems: ['One', 'Two', 'Three', 'Four', 'Five'],
+      electionItems: ["One", "Two", "Three", "Four", "Five"],
     }, // will be passed to the page component as props
-  }
+  };
 }
